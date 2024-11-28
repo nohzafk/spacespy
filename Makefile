7d1838e55acb49a06ad4aa1b27b39cb787ab4fa1
nohz.afk <149959021+nohzafk@users.noreply.github.com>
2024-11-28 21:07:11 +0100

    get the monitor name and space index correctly

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=clang
-CFLAGS=-framework Foundation -framework CoreGraphics -fobjc-arc -Wall
+CFLAGS=-framework Foundation -framework CoreGraphics -framework AppKit -fobjc-arc -Wall
 TARGET=spacespy
 
 all: $(TARGET)
--- spacespy.m
+++ spacespy.m
@@ -1,27 +1,41 @@
-// spacespy.m
 #import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <AppKit/AppKit.h>
 
 // Private CoreGraphics declarations
 typedef uint32_t CGSConnectionID;
 typedef uint32_t CGSSpaceID;
-typedef enum _CGSSpaceType {
-    kCGSSpaceUser,
-    kCGSSpaceSystem,
-} CGSSpaceType;
-
-typedef enum {
-    kCGSSpaceCurrent = 5,
-    kCGSSpaceAll = 7
-} CGSSpaceSelector;
-
-typedef int CGError;
 
 // External functions declarations
 extern CGSConnectionID CGSMainConnectionID(void);
-extern CGError CGSCopySpaces(CGSConnectionID cid, CGSSpaceSelector type, CGSSpaceID **spaces, int *count);
-extern CGError CGSGetSpaceType(CGSConnectionID cid, CGSSpaceID sid, CGSSpaceType *type);
 extern CFArrayRef CGSCopyManagedDisplaySpaces(const CGSConnectionID cid);
-extern CFArrayRef CGSCopySpacesForLevel(CGSConnectionID connection, int level);
+
+NSString* getDisplayName(NSString* displayID) {
+    // Get all screens
+    NSArray<NSScreen *> *screens = [NSScreen screens];
+    
+    for (NSScreen *screen in screens) {
+        NSDictionary *description = [screen deviceDescription];
+        NSNumber *screenID = description[@"NSScreenNumber"];
+        
+        if (screenID) {
+            // Get the display UUID
+            CGDirectDisplayID displayIDNum = [screenID unsignedIntValue];
+            CFUUIDRef uuid = CGDisplayCreateUUIDFromDisplayID(displayIDNum);
+            if (uuid) {
+                NSString *uuidString = (__bridge_transfer NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
+                CFRelease(uuid);
+                
+                if ([displayID isEqualToString:uuidString]) {
+                    // Get the localized name using the same method as Aerospace
+                    return [screen localizedName];
+                }
+            }
+        }
+    }
+    
+    return displayID;
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -33,31 +47,64 @@ int main(int argc, const char * argv[]) {
         
         if (displaySpaces) {
             NSArray *displays = (__bridge NSArray *)displaySpaces;
+            NSMutableDictionary *result = [NSMutableDictionary dictionary];
+            NSMutableArray *monitorsArray = [NSMutableArray array];
+
+            // Track global space index
+            __block int globalSpaceIndex = 1;
             
-            for (NSDictionary *display in displays) {
+            // Process displays in order of their arrangement
+            NSArray *orderedDisplays = [displays sortedArrayUsingComparator:^NSComparisonResult(NSDictionary *display1, NSDictionary *display2) {
+                NSArray *spaces1 = display1[@"Spaces"];
+                NSArray *spaces2 = display2[@"Spaces"];
+                NSNumber *firstSpaceId1 = spaces1.firstObject[@"id64"];
+                NSNumber *firstSpaceId2 = spaces2.firstObject[@"id64"];
+                return [firstSpaceId1 compare:firstSpaceId2];
+            }];
+            
+            for (NSDictionary *display in orderedDisplays) {
                 NSString *displayID = display[@"Display Identifier"];
                 NSArray *spaces = display[@"Spaces"];
                 NSNumber *currentSpace = display[@"Current Space"][@"id64"];
+                NSString *monitorName = getDisplayName(displayID);
                 
-                printf("Display: %s\n", [displayID UTF8String]);
+                NSMutableDictionary *monitorInfo = [NSMutableDictionary dictionary];
+                monitorInfo[@"name"] = monitorName;
+                monitorInfo[@"uuid"] = displayID;
+                NSMutableArray *spacesArray = [NSMutableArray array];
                 
-                int spaceIndex = 1;  // Start counting from 1 for each display
                 for (NSDictionary *space in spaces) {
                     NSNumber *spaceID = space[@"id64"];
                     NSNumber *managedSpaceID = space[@"ManagedSpaceID"];
+                    NSString *spaceUUID = space[@"uuid"];
+                    BOOL isCurrent = [spaceID isEqualToNumber:currentSpace];
                     
-                    // Mark current space with an asterisk
-                    const char *currentMarker = ([spaceID isEqualToNumber:currentSpace]) ? " *" : "";
+                    [spacesArray addObject:@{
+                        @"index": @(globalSpaceIndex),
+                        @"id": spaceID,
+                        @"managed_id": managedSpaceID,
+                        @"uuid": spaceUUID,
+                        @"is_current": @(isCurrent)
+                    }];
                     
-                    printf("  Desktop %d: Space ID: %lld, Managed Space ID: %lld%s\n",
-                           spaceIndex,
-                           [spaceID longLongValue],
-                           [managedSpaceID longLongValue],
-                           currentMarker);
-                    
-                    spaceIndex++;
+                    globalSpaceIndex++;
                 }
-                printf("\n");
+                
+                monitorInfo[@"spaces"] = spacesArray;
+                [monitorsArray addObject:monitorInfo];
+            }
+            
+            result[@"monitors"] = monitorsArray;
+            
+            // Convert to JSON and print
+            NSError *error = nil;
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:result
+                                                             options:NSJSONWritingPrettyPrinted
+                                                               error:&error];
+            
+            if (jsonData) {
+                NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+                printf("%s\n", [jsonString UTF8String]);
             }
             
             CFRelease(displaySpaces);
